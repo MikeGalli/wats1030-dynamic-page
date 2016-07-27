1fa9189ebc2a52b7043751c27cdc73de86465d7f
Mike Galligar <mkgalligar@gmail.com>
2016-07-27 17:54:47 -0400

    New dynamic droplet

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
+$API_KEY = '35edbca3494716bac72f7f2719ccb167'; // This will be your Flickr-issued API Key.
 $PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
 $SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
 
